40a97dfa4b83d9028f97493f2aa26a267f5b42d1
Chamaththa123 <shamodchamaththa@gmail.com>
2024-07-11 00:11:36 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,20 @@
-import express from "express";
-import colors from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoute.js";
-import cors from "cors";
-import bodyParser from "body-parser";
-import trafficRoutes from "./routes/trafficRoute.js";
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-//configure env
-dotenv.config();
-
-//databse config
-connectDB();
-
-//rest object
 const app = express();
+const port = 8000;
 
+const corsOptions = {
+    orign:"*"
+};
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(cookieParser());
 
-app.use("/api/v1/auth", authRoutes);
-app.use(trafficRoutes);
-
-//rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
-
-
-
-app.use(bodyParser.json());
-//PORT
-const PORT = process.env.PORT || 8080;
-
-//run listen
-app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
-});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
