81138ff8844dc5c51fbef71906ecb76d9cc799c1
Chamaththa123 <shamodchamaththa@gmail.com>
2024-07-13 02:17:10 +0530

    inventory servce setup

---
+++ backend/inventory-service/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = 8002;
+
+const corsOptions = {
+  orign: "*",
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+
+// app.use("/", require("./route/supplierRoute"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
